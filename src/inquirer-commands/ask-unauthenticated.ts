af4f650303676cd1afba44b0fcb18675e507d71d
DhrishP <parekhdhrish.pg@gmail.com>
2024-09-06 16:31:10 +0530

    add ask-unauthenticated.js

---
+++ src/inquirer-commands/ask-unauthenticated.ts
@@ -0,0 +1,13 @@
+import inquirer from "inquirer";
+
+export async function promptUnauthenticatedUser() {
+  const { action } = await inquirer.prompt<{ action: string }>([
+    {
+      type: "list",
+      name: "action",
+      message: "What would you like to do?",
+      choices: ["Login", "Register", "Reset Password", "Exit"],
+    },
+  ]);
+  return action;
+}
