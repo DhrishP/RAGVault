c6756b3d6d9fe842d5ceec7864d2bb9eaa3a170e
DhrishP <parekhdhrish.pg@gmail.com>
2024-09-09 16:29:49 +0530

    add headout.js for heading out of the app

---
+++ dist/inquirer-commands/nested-commands/headout.js
@@ -0,0 +1,12 @@
+import inquirer from "inquirer";
+export async function HeadOutActions() {
+    const { action } = await inquirer.prompt([
+        {
+            type: "list",
+            name: "action",
+            message: "What would you like to do?",
+            choices: ["Exit(Session will be saved)", "Logout", "Back"],
+        },
+    ]);
+    return action;
+}
---
+++ src/inquirer-commands/nested-commands/headout.ts
@@ -0,0 +1,13 @@
+import inquirer from "inquirer";
+
+export async function HeadOutActions() {
+  const { action } = await inquirer.prompt<{ action: string }>([
+    {
+      type: "list",
+      name: "action",
+      message: "What would you like to do?",
+      choices: ["Exit(Session will be saved)", "Logout", "Back"],
+    },
+  ]);
+  return action;
+}
