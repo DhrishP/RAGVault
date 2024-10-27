fc57daf0fd0b8ce790865ee33ba29aaee574b278
DhrishP <parekhdhrish.pg@gmail.com>
2024-10-28 00:36:57 +0530

    change ask-authenticated-action and added question commands

--- src/actions-for-inquirers/ask-authenticated-action.ts
+++ src/actions-for-inquirers/ask-authenticated-action.ts
@@ -1,5 +1,4 @@
 import chalk from "chalk";
-import inquirer from "inquirer";
 import { HeadOutCommands } from "../inquirer-commands/nested-commands/headout.js";
 import { HeadOutActions } from "./nested-actions-for-inquirers/headout-actions.js";
 import { SettingsCommands } from "../inquirer-commands/nested-commands/settings.js";
@@ -9,9 +8,9 @@ import { promptAuthenticatedUser } from "../inquirer-commands/ask-authenticated.
 import { LocalBrainActions } from "./nested-actions-for-inquirers/localbrain-actions.js";
 import { LocalBrainCommands } from "../inquirer-commands/nested-commands/localbrain.js";
 import { Session, UserStore } from "../types/index.js";
-import { getCollection } from "../utils/chroma-client.js";
-import { GetFireworksInstance } from "../utils/ai-providers.js";
-import { PromptTemplate } from "@langchain/core/prompts";
+import { QuestionActions } from "./nested-actions-for-inquirers/question.js";
+import { AskAQuestionCommands } from "../inquirer-commands/nested-commands/question.js";
+
 
 export async function handleAuthenticatedAction(
   action: string,
@@ -21,51 +20,15 @@ export async function handleAuthenticatedAction(
 ) {
   switch (action) {
     case "Ask a question🤔":
-      const collection = await getCollection(currentUserName + "-ragvault");
-      const { query } = await inquirer.prompt([
-        {
-          type: "input",
-          name: "query",
-          message: "Enter your query here",
-        },
-      ]);
-      const chunks = await collection.query({
-        queryTexts: [query],
-        nResults: 2,
-      });
-      const fireworks = GetFireworksInstance(
-        users[currentUserName].fireworksKey
-      );
-
-      const prompt = PromptTemplate.fromTemplate(
-        "You are a helpful assistant that can answer questions and help with tasks. You are given a question and a list of documents. You need to answer the question based on the given chunks of data. The chunks of data are: {context}. The question is: {question}.Do not recall that you are using chunks of data to answer the question. Talk like you are a human."
-      );
-      if (fireworks) {
-        try {
-          // console.log(chunks.documents.join("\n"));
-          const chain = prompt.pipe(fireworks);
-          const response = await chain.invoke({
-            context: chunks.documents.join(","),
-            question: query,
-          });
-          console.log(response.content);
-        } catch (error) {
-          console.log(error);
-        }
-      } else {
-        console.log(
-          chalk.red(
-            "Fireworks API key is not set. Please set your Fireworks API key and try again."
-          )
-        );
-      }
-      const AskAgainAction = await promptAuthenticatedUser(currentUserName);
-      await handleAuthenticatedAction(
-        AskAgainAction,
-        currentUserName,
-        session,
-        users
-      );
+      const question = await AskAQuestionCommands();
+      await QuestionActions(question, currentUserName, users, session);
+      // const AskAgainAction = await promptAuthenticatedUser(currentUserName);
+      // await handleAuthenticatedAction(
+      //   AskAgainAction,
+      //   currentUserName,
+      //   session,
+      //   users
+      // );
     case "Add data to your local brain🧠":
       const isChromaDockerRunning = await checkChromaDocker();
       if (isChromaDockerRunning) {
