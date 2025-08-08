937ba61cb248bdc4627c172543d5300f53487a43
DhrishP <parekhdhrish.pg@gmail.com>
2025-08-08 16:09:15 +0530

    fix : embedding function setting error

--- src/utils/chroma-client.ts
+++ src/utils/chroma-client.ts
@@ -5,29 +5,31 @@ import dotenv from "dotenv";
 import { OpenAIEmbeddingFunction } from "@chroma-core/openai";
 import { loadSession } from "./session.js";
 import { loadUsers } from "./user-transactions.js";
+import { DefaultEmbeddingFunction } from "@chroma-core/default-embed";
 
 dotenv.config();
 
 export const client = new ChromaClient({ host:"localhost",port:8765 });
-
-export const getCollection = async (collectionName: string) => {
-  const collection = client.getOrCreateCollection({ name: collectionName });
-  return collection;
-};
-
 export const embeddingFunction = async () => {
   const session = await loadSession();
   const users = await loadUsers();
   const currentUser = session.currentUser;
   if (currentUser && session.embeddingLLM === "openai") {
     const modelName = users[currentUser.username].embeddingModelName;
-    return new OpenAIEmbeddingFunction({
-      apiKey: users[currentUser.username].openAIKey || "",
-      modelName: modelName || "text-embedding-3-small",
-    });
+    const apiKey = users[currentUser.username].openAIKey;
+    if (modelName && apiKey) {
+      return new OpenAIEmbeddingFunction({
+        apiKey,
+        modelName,
+      });
+    }
   }
-  return new OpenAIEmbeddingFunction({
-    apiKey: process.env.OPENAI_API_KEY || "HI-TST-1",
-    modelName: "text-embedding-3-small",
-  });
+  return new DefaultEmbeddingFunction();
 };
+
+
+export const getCollection = async (collectionName: string) => {
+  const collection = client.getOrCreateCollection({ name: collectionName , embeddingFunction :await embeddingFunction()  });
+  return collection;
+};
+
